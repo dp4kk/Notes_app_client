0992454f2d3f9abd53e065ff9100a0116a001c9d
Deepak kushwaha <76859271+dp4kk@users.noreply.github.com>
2021-10-31 18:17:12 +0545

    Update CreateNote.js

--- src/Components/CreateNote.js
+++ src/Components/CreateNote.js
@@ -16,7 +16,7 @@ const CreateNote = () => {
 
     const handleSubmit=async(e)=>{
        e.preventDefault()
-        await axios
+         axios
           .post("https://reactnotes-app.herokuapp.com/savenote", {
             creator: currentUser.displayName,
             title: noteTopic,
